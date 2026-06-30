1d54f8d6e053e9665bce7954d7d0e3bbb65880ee
SyedHammadSherazi <144503092+SyedHammadSherazi@users.noreply.github.com>
2026-06-30 11:05:38 +0500

    django

--- newproject/newapp/models.py
+++ newproject/newapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Task(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    status=models.CharField(max_length=30)
+    due_date=models.DateField()
+    added_by=models.ForeignKey(Project,on_delete=models.CASCADE)
