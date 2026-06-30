686b8cf6610ade926a369d6134de47f3bb81fa5a
SyedHammadSherazi <144503092+SyedHammadSherazi@users.noreply.github.com>
2026-06-30 12:32:07 +0500

    Models & the ORM

--- newproject/newapp/admin.py
+++ newproject/newapp/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from .models import Project, Task
 
-# Register your models here.
-from.models import Project, Task
-admin.site.register(Project)
-admin.site.register(Task)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "start_date", "end_date")
+    list_per_page = 1
+    search_fields = ("name",)
+    list_editable = ("name",)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "due_date", "added_by")
+   
--- newproject/newapp/models.py
+++ newproject/newapp/models.py
@@ -6,10 +6,26 @@ class Project(models.Model):
     description=models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
 
 class Task(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField()
-    status=models.CharField(max_length=30)
+    STATUS_CHOICES = [
+    ("pending", "Pending"),
+    ("in-progress", "In Progress"),
+    ("completed", "Completed"),
+]
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="pending"
+    )
     due_date=models.DateField()
     added_by=models.ForeignKey(Project,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
